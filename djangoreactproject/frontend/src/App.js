f45a6a7bd4c4395fd169297b5b7235c00ca4f5f5
Victor Willian de Sousa <victorwillian1012@gmail.com>
2021-04-24 15:25:10 -0300

    Initial commit

---
+++ djangoreactproject/frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        title="Mind Map"
+        style={{ border: "none", width: 800, height: 450 }}
+        src="https://whimsical.com/embed/7mryvEeJLGQvHNnQaqtKK5@2Ux7TurymMYa1akxVAtE"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
