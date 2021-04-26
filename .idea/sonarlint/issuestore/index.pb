196198987d275437fc3fd0750a66449492dfb97c
V1ctorW1ll1an <victorwillian1012@gmail.com>
2021-04-26 18:17:35 -0300

    ide configs

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,7 @@
 
-7
-main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
+]
+-djangoreactproject/djangoreactproject/asgi.py,3/9/391d8098ede87ddc01a43c18e72bb2c38d33485a
+‚
+Rdjangoreactproject/venv/lib/python3.9/site-packages/django/core/checks/registry.py,5/e/5ef30e10d7ef0db935fca00078bd0bfa0f351727
+]
+-djangoreactproject/djangoreactproject/wsgi.py,e/7/e7e4d78351a5ce4fc1d443d3f60ce212b2c64dbc
--- djangoreactproject/djangoreactproject/urls.py
+++ djangoreactproject/djangoreactproject/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from customers.views import create_customer, update_customer, get_all_customers, get_customer_by_pk, delete_customer
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/customers/getall/$', get_all_customers.GetAllCustomers.as_view()),
+
+    url(r'^api/customers/create/$', create_customer.CreateCustomer.as_view()),
+    url(r'^api/customers/update/(?P<pk>[0-9]+)/$', update_customer.UpdateCustomer.as_view()),
+
+    url(r'^api/customers/getbypk/(?P<pk>[0-9]+)/$', get_customer_by_pk.GetCustomerByPk.as_view()),
+    url(r'^api/customers/delete/(?P<pk>[0-9]+)/$', delete_customer.DeleteCustomer.as_view()),
 ]
